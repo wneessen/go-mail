8f96251ebf7208947bd599370781286f1e08c982
Winni Neessen <winfried.neessen@cleverbridge.com>
2022-03-20 19:31:30 +0100

    Update auth_test.go to cover the new response

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -17,8 +17,8 @@ func TestAuth(t *testing.T) {
 
 	authTests := []authTest{
 		{LoginAuth("user", "pass", "testserver"),
-			[]string{"Username:", "Password:", "Invalid:"}, "LOGIN",
-			[]string{"", "user", "pass", ""}, []bool{false, false, true}},
+			[]string{"Username:", "Password:", "2.7.0 Authentication successful", "Invalid:"}, "LOGIN",
+			[]string{"", "user", "pass", "", ""}, []bool{false, false, false, true}},
 	}
 
 testLoop:
