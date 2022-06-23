fdc9c86b467f816597f1fc4a327268c755819c50
Winni Neessen <winfried.neessen@cleverbridge.com>
2022-06-23 16:16:25 +0200

    #28: DKIM implementation
    Fix SPDX header

--- dkim/canonicalization.go
+++ dkim/canonicalization.go
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Winni Neessen <winni@neessen.dev>
+//
+// SPDX-License-Identifier: MIT
+
 package dkim
 
 // Canonicalization implements the different Canonicalization types as described in
