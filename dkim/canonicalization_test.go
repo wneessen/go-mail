245f37f1c616f42cadd3445db3f90f518095f146
Winni Neessen <winfried.neessen@cleverbridge.com>
2022-06-23 16:30:11 +0200

    #28: DKIM implementation
    * Make Linter happy

--- dkim/canonicalization_test.go
+++ dkim/canonicalization_test.go
@@ -5,7 +5,6 @@
 package dkim
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -22,7 +21,7 @@ func TestCanonicalization_String(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.n, func(t *testing.T) {
-			cs := fmt.Sprintf("%s", tt.c)
+			cs := tt.c.String()
 			if cs != tt.e {
 				t.Errorf("Canonizaliation to string conversion failed. Expected: %s, got: %s", tt.e, cs)
 			}
