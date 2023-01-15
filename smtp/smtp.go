3922d3f195a8a37987445119406c49699f0a4e02
Winni Neessen <winfried.neessen@cleverbridge.com>
2023-01-15 16:14:19 +0100

    Switched copyright header from me to "The go-mail Authors"

--- smtp/smtp.go
+++ smtp/smtp.go
@@ -17,13 +17,6 @@
 //	8BITMIME  RFC 1652
 //	AUTH      RFC 2554
 //	STARTTLS  RFC 3207
-//
-// Additional extensions may be handled by clients.
-//
-// The smtp package is frozen and is not accepting new features.
-// Some external packages provide more functionality. See:
-//
-//	https://godoc.org/?q=smtp
 package smtp
 
 import (
