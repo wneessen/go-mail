49a2e31ae928fe1bf1aa4919093bdcae3cb5dd51
Winni Neessen <wn@neessen.dev>
2025-09-06 11:30:01 +0000

    Refactor codebase to align with updated Go version support
    - Removed Go 1.20 specific files and tests to clean up legacy code.
    - Consolidated tests for newer Go versions to streamline maintainability.
    - Updated logging and debug-related test cases to improve clarity and consistency.
    - Migrated `SendWithSMTPClient` method implementation and documentation to a new consolidated structure.

--- smtp/smtp_test.go
+++ smtp/smtp_test.go
@@ -3302,6 +3302,39 @@ func TestClient_SetDebugLog(t *testing.T) {
 	})
 }
 
+func TestClient_SetDebugLog_JSON(t *testing.T) {
+	t.Run("set debug loggging to on should not override logger", func(t *testing.T) {
+		client := &Client{logger: log.NewJSON(os.Stderr, log.LevelDebug)}
+		client.SetDebugLog(true)
+		if !client.debug {
+			t.Fatalf("expected debug log to be true")
+		}
+		if client.logger == nil {
+			t.Fatalf("expected logger to be defined")
+		}
+		if !strings.EqualFold(fmt.Sprintf("%T", client.logger), "*log.JSONlog") {
+			t.Errorf("expected logger to be of type *log.JSONlog, got: %T", client.logger)
+		}
+	})
+}
+
+func TestClient_SetLogger_JSON(t *testing.T) {
+	t.Run("set logger to JSONlog logger", func(t *testing.T) {
+		client := &Client{}
+		client.SetLogger(log.NewJSON(os.Stderr, log.LevelDebug))
+		if !strings.EqualFold(fmt.Sprintf("%T", client.logger), "*log.JSONlog") {
+			t.Errorf("expected logger to be of type *log.JSONlog, got: %T", client.logger)
+		}
+	})
+	t.Run("nil logger should just return and not set/override", func(t *testing.T) {
+		client := &Client{logger: log.NewJSON(os.Stderr, log.LevelDebug)}
+		client.SetLogger(nil)
+		if !strings.EqualFold(fmt.Sprintf("%T", client.logger), "*log.JSONlog") {
+			t.Errorf("expected logger to be of type *log.JSONlog, got: %T", client.logger)
+		}
+	})
+}
+
 func TestClient_SetLogger(t *testing.T) {
 	t.Run("set logger to Stdlog logger", func(t *testing.T) {
 		client := &Client{}
