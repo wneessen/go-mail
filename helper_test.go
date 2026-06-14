f98915dde859218afcda5c9798a7f22181400c64
Winni Neessen <wn@neessen.dev>
2026-06-14 17:59:52 +0200

    Make getGoVersion accept incMinorVer flag
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- helper_test.go
+++ helper_test.go
@@ -13,14 +13,20 @@ import (
 
 // getGoVersion returns the current Go runtime version as a float64. It fails the test if the version
 // parsing encounters an error.
-func getGoVersion(t *testing.T) float64 {
+func getGoVersion(t *testing.T, incMinorVer bool) float64 {
 	t.Helper()
 	version := runtime.Version()
-	version = version[2:]
-	version = version[:strings.LastIndex(version, ".")]
-	vernum, err := strconv.ParseFloat(version, 64)
+	parts := strings.Split(version[2:], ".")
+	majorVerNum, err := strconv.ParseFloat(parts[0]+"."+parts[1], 64)
 	if err != nil {
 		t.Fatalf("failed to parse Go version: %s", err)
 	}
-	return vernum
+	minorVerNum, err := strconv.ParseFloat(parts[0]+"."+parts[1]+parts[2], 64)
+	if err != nil {
+		t.Fatalf("failed to parse Go version: %s", err)
+	}
+	if incMinorVer {
+		return minorVerNum
+	}
+	return majorVerNum
 }
