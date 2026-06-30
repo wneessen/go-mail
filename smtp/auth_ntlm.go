a5e32da8163b9dbd621ddec219461b58302d39a1
Winni Neessen <wn@neessen.dev>
2026-06-30 09:57:55 +0200

    Refactor NTLM auth: rename type and simplify flow
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- smtp/auth_ntlm.go
+++ smtp/auth_ntlm.go
@@ -6,9 +6,9 @@ import (
 	"github.com/wneessen/go-mail/internal/ntlm"
 )
 
-// NTLMCustomAuth implements the NTLM authentication mechanism for SMTP.
+// ntlmAuth implements the NTLM authentication mechanism for SMTP.
 // It holds the necessary information to perform NTLM authentication.
-type NTLMCustomAuth struct {
+type ntlmAuth struct {
 	// session is the NTLM client session used for authentication.
 	session *ntlm.NTLMv2Session
 
@@ -35,36 +35,21 @@ type NTLMCustomAuth struct {
 //   - toServer: A byte slice containing the NTLM negotiate message to be sent to the server.
 //   - err: An error if the NTLM authentication method is not supported or if there's an issue
 //     creating the NTLM session or generating the negotiation message.
-func (a *NTLMCustomAuth) Start(info *ServerInfo) (proto string, toServer []byte, err error) {
-	if len(info.Auth) > 0 {
-		// check NTLM support in the server's authentication methods
-		supported := false
-		for _, method := range info.Auth {
-			if method == "NTLM" {
-				supported = true
-			}
-		}
-		if !supported {
-			return "", nil, fmt.Errorf("NTLM auth method not supported")
-		}
+func (a *ntlmAuth) Start(info *ServerInfo) (proto string, toServer []byte, err error) {
+	// Initialize a NTLMv2 client session and assign username, password and domain
+	a.session = ntlm.CreateClientSession()
+	a.session.SetUserInfo(a.UserName, a.Password, a.Domain)
 
-		// create and configure the NTLM client session
-		a.session = ntlm.CreateClientSession()
-		if err != nil {
-			return "", nil, fmt.Errorf("failed to create NTLM client session: %v", err)
-		}
-		a.session.SetUserInfo(a.UserName, a.Password, a.Domain)
-		negotiate, err := a.session.GenerateNegotiateMessage()
-		if err != nil {
-			return "", nil, fmt.Errorf("failed to generate negotiate message: %v", err)
-		}
-		return "NTLM", negotiate.Bytes(), nil
-	}
-	return "NTLM", nil, nil
+	// Initialize a NTLMSSP Type 1 message, which holds the negotiation information
+	// for the NTLM authentication.
+	//
+	// See: https://curl.se/rfc/ntlm.html#theType1Message
+	negotiate, err := a.session.GenerateNegotiateMessage()
+	return "NTLM", negotiate.Bytes(), nil
 }
 
-// Next continues the NTLM authentication process by handling the challenge from the server.
-// It parses the challenge message, processes it, and generates an "authenticate" message in response.
+// Next continues the NTLM authentication process by parsing the Type 2 message from the
+// server followed by generating and sending the final Type 3 authentication message.
 //
 // Parameters:
 //   - challengeBytes: A byte slice containing the NTLM challenge message from the server.
@@ -75,20 +60,22 @@ func (a *NTLMCustomAuth) Start(info *ServerInfo) (proto string, toServer []byte,
 //     or nil if no further authentication steps are needed.
 //   - err: An error if there's an issue parsing the challenge, processing it, or generating
 //     the "authenticate" message. Returns nil if successful or if no further steps are needed.
-func (a *NTLMCustomAuth) Next(challengeBytes []byte, more bool) (toServer []byte, err error) {
-	// continue the authentication process if more steps are expected (more = true)
+func (a *ntlmAuth) Next(challengeBytes []byte, more bool) (toServer []byte, err error) {
 	if more {
-		// parse the challenge message and process it
+		// Parse and process the server challenge message (Type 2 message)
+		//
+		// See: https://curl.se/rfc/ntlm.html#theType2Message
 		challenge, err := ntlm.ParseChallengeMessage(challengeBytes)
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse challenge message: %v", err)
+			return nil, fmt.Errorf("failed to parse challenge message: %w", err)
 		}
-		err = a.session.ProcessChallengeMessage(challenge)
-		if err != nil {
-			return nil, fmt.Errorf("failed to process challenge message: %v", err)
+		if err = a.session.ProcessChallengeMessage(challenge); err != nil {
+			return nil, fmt.Errorf("failed to process challenge message: %w", err)
 		}
 
-		// generate the authenticate message in response to the challenge
+		// Generate the authentication message (Type 3 message) and return it to the server
+		//
+		// See: https://curl.se/rfc/ntlm.html#theType3Message
 		authenticate := a.session.GenerateAuthenticateMessage()
 		return authenticate.Bytes(), nil
 	}
@@ -96,8 +83,10 @@ func (a *NTLMCustomAuth) Next(challengeBytes []byte, more bool) (toServer []byte
 	return nil, nil
 }
 
-func NewNTLMAuth(user, pass, domain string) Auth {
-	return &NTLMCustomAuth{
+// NTLMAuth returns an [Auth] that implements the NTLM authentication
+// mechanism using the NTLMSSP protocol.
+func NTLMAuth(user, pass, domain string) Auth {
+	return &ntlmAuth{
 		UserName: user,
 		Password: pass,
 		Domain:   domain,
